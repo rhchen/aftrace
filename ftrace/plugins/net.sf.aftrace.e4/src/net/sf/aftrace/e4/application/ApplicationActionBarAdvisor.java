22cb609eb84d578e9708804387e1d9c353c55c16
ranhow.chen <ranhow.chen@gmail.com>
2013-11-04 00:56:49 +0800

    Add project explorer support
    	modified:   ftrace/features/net.sf.aftrace.feature/feature.xml
    	modified:   ftrace/plugins/net.sf.aftrace.e4/Application.e4xmi
    	modified:   ftrace/plugins/net.sf.aftrace.e4/META-INF/MANIFEST.MF
    	new file:   ftrace/plugins/net.sf.aftrace.e4/icons/alt_about.gif
    	new file:   ftrace/plugins/net.sf.aftrace.e4/icons/alt_launcher.icns
    	new file:   ftrace/plugins/net.sf.aftrace.e4/icons/alt_launcher.ico
    	new file:   ftrace/plugins/net.sf.aftrace.e4/icons/alt_launcher.xpm
    	new file:   ftrace/plugins/net.sf.aftrace.e4/icons/alt_window_16.gif
    	new file:   ftrace/plugins/net.sf.aftrace.e4/icons/alt_window_32.gif
    	modified:   ftrace/plugins/net.sf.aftrace.e4/net.sf.aftrace.e4.product
    	modified:   ftrace/plugins/net.sf.aftrace.e4/plugin.xml
    	new file:   ftrace/plugins/net.sf.aftrace.e4/src/net/sf/aftrace/e4/application/Application.java
    	new file:   ftrace/plugins/net.sf.aftrace.e4/src/net/sf/aftrace/e4/application/ApplicationActionBarAdvisor.java
    	new file:   ftrace/plugins/net.sf.aftrace.e4/src/net/sf/aftrace/e4/application/ApplicationWorkbenchAdvisor.java
    	new file:   ftrace/plugins/net.sf.aftrace.e4/src/net/sf/aftrace/e4/application/ApplicationWorkbenchWindowAdvisor.java
    	new file:   ftrace/plugins/net.sf.aftrace.e4/src/net/sf/aftrace/e4/application/Perspective.java

---
+++ ftrace/plugins/net.sf.aftrace.e4/src/net/sf/aftrace/e4/application/ApplicationActionBarAdvisor.java
@@ -0,0 +1,20 @@
+package net.sf.aftrace.e4.application;
+
+import org.eclipse.jface.action.IMenuManager;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.application.ActionBarAdvisor;
+import org.eclipse.ui.application.IActionBarConfigurer;
+
+public class ApplicationActionBarAdvisor extends ActionBarAdvisor {
+
+    public ApplicationActionBarAdvisor(IActionBarConfigurer configurer) {
+        super(configurer);
+    }
+
+    protected void makeActions(IWorkbenchWindow window) {
+    }
+
+    protected void fillMenuBar(IMenuManager menuBar) {
+    }
+    
+}
