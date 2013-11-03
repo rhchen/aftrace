cd97a3d9ca9c932edbbbbd54face3c7314ea444d
ranhow.chen <ranhow.chen@gmail.com>
2013-11-04 00:58:40 +0800

    Fix app init
    	modified:   ftrace/plugins/net.sf.aftrace.e4/src/net/sf/aftrace/e4/application/ApplicationWorkbenchAdvisor.java

--- ftrace/plugins/net.sf.aftrace.e4/src/net/sf/aftrace/e4/application/ApplicationWorkbenchAdvisor.java
+++ ftrace/plugins/net.sf.aftrace.e4/src/net/sf/aftrace/e4/application/ApplicationWorkbenchAdvisor.java
@@ -28,34 +28,20 @@ public class ApplicationWorkbenchAdvisor extends WorkbenchAdvisor {
 
 	@Override
 	public void initialize(IWorkbenchConfigurer configurer) {
-//		super.initialize(configurer);
-//		// inserted: register workbench adapters
-//		IDE.registerAdapters();
-//
-//		// inserted: register images for rendering explorer view
-//		final String ICONS_PATH = "icons/full/";
-//		final String PATH_OBJECT = ICONS_PATH + "obj16/";
-//		Bundle ideBundle = Platform.getBundle(IDEWorkbenchPlugin.IDE_WORKBENCH);
-//		declareWorkbenchImage(configurer, ideBundle,
-//				IDE.SharedImages.IMG_OBJ_PROJECT, PATH_OBJECT + "prj_obj.gif",
-//				true);
-//		declareWorkbenchImage(configurer, ideBundle,
-//				IDE.SharedImages.IMG_OBJ_PROJECT_CLOSED, PATH_OBJECT
-//						+ "cprj_obj.gif", true);
-		
-		 IDE.registerAdapters();
-         final String ICONS_PATH = "icons/full/";
-         final String PATH_OBJECT = ICONS_PATH + "obj16/";
-         Bundle ideBundle = Platform.getBundle(IDEWorkbenchPlugin.IDE_WORKBENCH);
-         declareWorkbenchImage(configurer, ideBundle,
-                         IDE.SharedImages.IMG_OBJ_PROJECT, PATH_OBJECT + "prj_obj.gif",
-                         true);
-         declareWorkbenchImage(configurer, ideBundle,
-                         IDE.SharedImages.IMG_OBJ_PROJECT_CLOSED, PATH_OBJECT
-                                         + "cprj_obj.gif", true);
+		super.initialize(configurer);
+		// inserted: register workbench adapters
+		IDE.registerAdapters();
 
-         super.initialize(configurer);
-         configurer.setSaveAndRestore(true);
+		// inserted: register images for rendering explorer view
+		final String ICONS_PATH = "icons/full/";
+		final String PATH_OBJECT = ICONS_PATH + "obj16/";
+		Bundle ideBundle = Platform.getBundle(IDEWorkbenchPlugin.IDE_WORKBENCH);
+		declareWorkbenchImage(configurer, ideBundle,
+				IDE.SharedImages.IMG_OBJ_PROJECT, PATH_OBJECT + "prj_obj.gif",
+				true);
+		declareWorkbenchImage(configurer, ideBundle,
+				IDE.SharedImages.IMG_OBJ_PROJECT_CLOSED, PATH_OBJECT
+						+ "cprj_obj.gif", true);
 	}
 
 	private void declareWorkbenchImage(IWorkbenchConfigurer configurer_p,
