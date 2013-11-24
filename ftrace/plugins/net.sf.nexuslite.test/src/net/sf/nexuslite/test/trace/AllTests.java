89015c1a5a86a731a355ba5685cfb2adc6268fee
ranhow.chen <ranhow.chen@gmail.com>
2013-11-24 16:59:47 +0800

    Add Nexuslite unit test
    	new file:   ftrace/plugins/net.sf.nexuslite.test/.classpath
    	new file:   ftrace/plugins/net.sf.nexuslite.test/.gitignore
    	new file:   ftrace/plugins/net.sf.nexuslite.test/.project
    	new file:   ftrace/plugins/net.sf.nexuslite.test/.settings/org.eclipse.jdt.core.prefs
    	new file:   ftrace/plugins/net.sf.nexuslite.test/META-INF/MANIFEST.MF
    	new file:   ftrace/plugins/net.sf.nexuslite.test/build.properties
    	new file:   ftrace/plugins/net.sf.nexuslite.test/samples/nexusTrace
    	new file:   ftrace/plugins/net.sf.nexuslite.test/src/net/sf/nexuslite/test/Activator.java
    	new file:   ftrace/plugins/net.sf.nexuslite.test/src/net/sf/nexuslite/test/trace/AllTests.java
    	new file:   ftrace/plugins/net.sf.nexuslite.test/src/net/sf/nexuslite/test/trace/NexusTraceTest.java
    	modified:   ftrace/plugins/net.sf.nexuslite/META-INF/MANIFEST.MF

---
+++ ftrace/plugins/net.sf.nexuslite.test/src/net/sf/nexuslite/test/trace/AllTests.java
@@ -0,0 +1,11 @@
+package net.sf.nexuslite.test.trace;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ NexusTraceTest.class })
+public class AllTests {
+
+}
---
+++ ftrace/plugins/net.sf.nexuslite.test/src/net/sf/nexuslite/test/trace/NexusTraceTest.java
@@ -0,0 +1,121 @@
+package net.sf.nexuslite.test.trace;
+
+import static org.junit.Assert.*;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.Vector;
+
+import net.sf.nexuslite.NexusTrace;
+import net.sf.nexuslite.test.Activator;
+
+import org.eclipse.core.runtime.FileLocator;
+import org.eclipse.core.runtime.Path;
+import org.eclipse.linuxtools.internal.tmf.core.component.TmfProviderManager;
+import org.eclipse.linuxtools.tmf.core.component.ITmfDataProvider;
+import org.eclipse.linuxtools.tmf.core.event.ITmfEvent;
+import org.eclipse.linuxtools.tmf.core.event.ITmfEventField;
+import org.eclipse.linuxtools.tmf.core.exceptions.TmfTraceException;
+import org.eclipse.linuxtools.tmf.core.request.TmfEventRequest;
+import org.eclipse.linuxtools.tmf.core.timestamp.TmfTimeRange;
+import org.eclipse.linuxtools.tmf.core.timestamp.TmfTimestamp;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class NexusTraceTest {
+
+	private static final long DEFAULT_INITIAL_OFFSET_VALUE = (1L * 100 * 1000 * 1000); // .1sec
+	private static final int BLOCK_SIZE = 500;
+	private static final int NB_EVENTS = 10000;
+	private static NexusTrace fTrace = null;
+
+	private static int SCALE = -3;
+
+	@Before
+	public void setUp() throws Exception {
+		
+		setupTrace("samples//nexusTrace");
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	//@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testProcessEventRequestForAllEvents()
+			throws InterruptedException {
+		
+		final int blockSize = 1;
+		
+		final Vector<ITmfEvent> requestedEvents = new Vector<ITmfEvent>();
+
+		final TmfTimeRange range = new TmfTimeRange(TmfTimestamp.BIG_BANG, TmfTimestamp.BIG_CRUNCH);
+		
+		final TmfEventRequest request = new TmfEventRequest(ITmfEvent.class,
+				range, NB_EVENTS, blockSize) {
+			@Override
+			public void handleData(final ITmfEvent event) {
+				super.handleData(event);
+				requestedEvents.add(event);
+				//System.out.println("event : "+ event.toString());
+			}
+		};
+		
+		final ITmfDataProvider[] providers = TmfProviderManager.getProviders(ITmfEvent.class, NexusTrace.class);
+		
+		providers[0].sendRequest(request);
+		
+		request.waitForCompletion();
+
+		assertEquals("nbEvents", NB_EVENTS, requestedEvents.size());
+		assertTrue("isCompleted", request.isCompleted());
+		assertFalse("isCancelled", request.isCancelled());
+
+		for (int i = 0; i < NB_EVENTS; i++) {
+			
+			 long timestamp = requestedEvents.get(i).getTimestamp().getValue();
+			 ITmfEventField field = requestedEvents.get(i).getContent().getField("value");
+			 int value = (Integer) field.getValue();
+			 
+			 //System.out.println("event : "+ timestamp +" "+ value);
+		}
+	}
+	
+	private static NexusTrace setupTrace(final String path) {
+        
+		if (fTrace == null) {
+           
+			try {
+               
+				final URL location = FileLocator.find(Activator.getDefault().getBundle(), new Path(path), null);
+                
+				final File test = new File(FileLocator.toFileURL(location).toURI());
+                
+				fTrace = new NexusTrace();
+                
+				fTrace.initTrace(null, test.toURI().getPath(), ITmfEvent.class);
+				
+				fTrace.indexTrace(true);
+				
+            } catch (final TmfTraceException e) {
+                e.printStackTrace();
+            } catch (final URISyntaxException e) {
+                e.printStackTrace();
+            } catch (final IOException e) {
+                e.printStackTrace();
+            } catch (final Exception e){
+            	e.printStackTrace();
+            }
+        }
+		
+        return fTrace;
+    }
+}
