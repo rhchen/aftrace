e58fc9516f60b7f54b88099e813c0c6940257e30
ranhow.chen <ranhow.chen@gmail.com>
2014-02-16 21:45:02 +0800

    Add net.tourbook.chart.test mvn setting
    	new file:   libs/plugins/net.tourbook.chart.test/.classpath
    	new file:   libs/plugins/net.tourbook.chart.test/.gitignore
    	new file:   libs/plugins/net.tourbook.chart.test/.project
    	new file:   libs/plugins/net.tourbook.chart.test/.settings/org.eclipse.jdt.core.prefs
    	new file:   libs/plugins/net.tourbook.chart.test/.settings/org.eclipse.m2e.core.prefs
    	new file:   libs/plugins/net.tourbook.chart.test/META-INF/MANIFEST.MF
    	new file:   libs/plugins/net.tourbook.chart.test/build.properties
    	new file:   libs/plugins/net.tourbook.chart.test/pom.xml
    	new file:   libs/plugins/net.tourbook.chart.test/src/net/tourbook/chart/test/ChartTest.java

---
+++ libs/plugins/net.tourbook.chart.test/src/net/tourbook/chart/test/ChartTest.java
@@ -0,0 +1,58 @@
+/**
+ * 
+ */
+package net.tourbook.chart.test;
+
+import static org.junit.Assert.*;
+import junit.framework.Assert;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * @author admin
+ *
+ */
+public class ChartTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link net.tourbook.chart.Chart#Chart(org.eclipse.swt.widgets.Composite, int)}.
+	 */
+	@SuppressWarnings("deprecation")
+	@Test
+	public final void testChart() {
+		Assert.assertEquals(true, true);
+	}
+
+}
